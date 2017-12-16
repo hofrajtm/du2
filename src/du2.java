b67e852ee21a9cc1d863a7d56bd8c0ba51a1f252
Martin <Martin@Martin-Lenovo>
2017-12-16 15:11:21 +0100

    Desaty pokus

--- src/du2.java
+++ src/du2.java
@@ -8,156 +8,70 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import static java.lang.Math.sqrt;
 import java.util.Locale;
-import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import static jdk.nashorn.internal.runtime.regexp.joni.Syntax.Java;
 
-/**
- * třída na správu argumentů z příkazové řádky
- * @author Martin
- */
-class Arguments {
-    // argumenty programu
+// třída na správu jednotlivých argumentů programu z příkazového řádku
+class Argumenty {
     public int p; // exponent ve váhové funkci
-    public int s;
-    public int sirka;
-    public int vyska;
-    public String in;
-    public String out;
+    public int s; // vyhlazovací koeficient
+    public int sirka; // velikost mřížky (šířka)
+    public int vyska; // velikost mřížky (výška)
+    public String in; // vstupní soubor
+    public String out; // výstupní soubor
 
-    public Arguments(String[] args) {
-        int arguments_length = args.length;
+    public Argumenty(String[] args) {
+        int argumenty_pocet = args.length;
 
-        if (arguments_length < 2) {
-            throw new RuntimeException("The length of arguments should be at least 2, in and out file.");
+        if (argumenty_pocet < 2) {
+            System.err.println("Minimální počet argumentů jsou dva - vstupní a výstupní soubor.");
+            System.exit(1);
         }
-
+        
         this.p = 2;
         this.s = 0;
         this.sirka = 100;
         this.vyska = 100;
-        this.in = args[arguments_length - 2];
-        this.out = args[arguments_length - 1];
+        this.in = args[argumenty_pocet - 2]; // pozice vstupního souboru v argumentu
+        this.out = args[argumenty_pocet - 1]; // pozice výstupního souboru v argumentu
 
-        for (int i = 0; i < (arguments_length - 2) / 2; i++) {
+        for (int i = 0; i < (argumenty_pocet - 2) / 2; i++) {
             if (args[i * 2].equals("-p")) {
                 this.p = Integer.parseInt(args[i * 2 + 1]); // funkce na přeměnu stringu na integer
             } else if (args[i * 2].equals("-s")) {
                 this.s = Integer.parseInt(args[i * 2 + 1]);
             } else if (args[i * 2].equals("-g")) {
-                String[] sirkaVyska = args[i * 2 + 1].split("x");
+                String[] sirkaVyska = args[i * 2 + 1].split("x"); // argument velikosti mřížky je rozdělen podle 'x'
                 this.sirka = Integer.parseInt(sirkaVyska[0]);
                 this.vyska = Integer.parseInt(sirkaVyska[1]);
             } else {
-                // doplnit chybovou hlasku
+                System.err.println("Argumenty programu nebyly zadány správně.");
+                System.exit(1);
             }
         }
     }
 }
 
-/**
- *
- * @author Martin
- */
 public class du2 {
-
+    // nastavení počátečních limitních hodnot minim a maxim 'x' a 'y' bodů ze vstupního souboru, pro určení mezí interpolace
     static double MIN_X = Double.POSITIVE_INFINITY;
     static double MAX_X = Double.NEGATIVE_INFINITY;
     static double MIN_Y = Double.POSITIVE_INFINITY;
     static double MAX_Y = Double.NEGATIVE_INFINITY;
 
-    /**
-     * @param args the command line arguments
-     * @throws java.io.IOException
-     */
     public static void main(String[] args) throws IOException {
-        // nastaví na anglickou lokaci, např. desetinná čárka se změní na tečku
-        Locale.setDefault(new Locale("en", "US"));
-        Arguments arguments = new Arguments(args); //načte parametry z příkazové řádky 
+        Locale.setDefault(new Locale("en", "US")); // nastaví na anglickou lokaci, např. desetinná čárka se změní na tečku
+        Argumenty arguments = new Argumenty(args); // načte parametry z příkazové řádky 
         double[][] data = nactiVstupniSoubor();
-        vypisVystup(arguments, data);
+        ulozVystupniSoubor(arguments, data);
     }
 
-    /**
-     *
-     * @param arguments Nactena trida argumentu
-     * @param data Vsechna data ze vstupniho souboru.
-     */
-    public static void vypisVystup(Arguments arguments, double[][] data) {
-        // uložení hodnot do určeného souboru
-        PrintWriter writer;
-        try {
-            writer = new PrintWriter("out.csv");
-            writer.println(arguments.sirka * arguments.vyska);
-            // projde celý řádek od min s krokem dle definované hodnoty mřížky 'sirka' po max
-            for (double x = MIN_X; x <= MAX_X; x += (MAX_X - MIN_X) / arguments.sirka) {
-                // projde všechny sloupce od min s krokem dle definované hodnoty mřížky 'vyska' po max
-                for (double y = MIN_Y; y <= MAX_Y; y += (MAX_Y - MIN_Y) / arguments.vyska) {
-
-                    double z0 = spoctiIDW(data, x, y, arguments);
-                    writer.printf("%.2f,", z0);
-                }
-                writer.printf("\n");
-            }
-            writer.close();
-        } catch (FileNotFoundException ex) {
-            System.err.format("Soubor %s nebyl nalezen!", "out.csv");
-            System.exit(1);
-        } catch (IOException ex) {
-            System.err.print("Vyskytla se chyba při ukládání hodnot do řádku!");
-            System.exit(1);
-        }
-    }
-
-    /**
-     *
-     * @param data
-     * @param x
-     * @param y
-     * @param arguments
-     * @return
-     */
-    public static double spoctiIDW(double[][] data, double x, double y, Arguments arguments) {
-        double lambda_jmenovatel = spoctiJmenovatel(data, x, y, arguments);
-        //výpočet čitatele lambdy
-        double z0 = spoctiVysledek(data, x, y, arguments, lambda_jmenovatel); //writer.printf("%.2f, %.2f, %.2f\n", x, y, z0);
-        return z0;
-    }
-
-    /**
-     *
-     * @param data
-     * @param x
-     * @param y
-     * @param arguments
-     * @return
-     */
-    public static double spoctiJmenovatel(double[][] data, double x, double y, Arguments arguments) {
-        double lambda_jmenovatel = 0;
-        // výpočet jmenovatele lambdy
-        for (int i = 0; i < data.length; i++) {
-            double v = sqrt((x - data[i][0]) * (x - data[i][0]) + (y - data[i][1]) * (y - data[i][1]));
-            lambda_jmenovatel += 1 / Math.pow(v, arguments.p);
-        }
-        return lambda_jmenovatel;
-    }
-
-    /**
-     *
-     * @return
-     * @throws NumberFormatException
-     */
     public static double[][] nactiVstupniSoubor() throws NumberFormatException {
         double[][] data = null;
         try {
             // načtení hodnot z určeného souboru
             BufferedReader br = new BufferedReader(new FileReader("in.csv"));
-            // definuje proměnné min a max 'x' a 'y'
             // načte počet řádku dle čísla v prvním řádku
             int pocet_radku = Integer.parseInt(br.readLine());
             // pole polí obasahující pozici [sloupce] a [řádku]
@@ -171,14 +85,7 @@ public class du2 {
         }
         return data;
     }
-
-    /**
-     *
-     * @param pocet_radku
-     * @param br
-     * @return
-     * @throws IOException
-     */
+    
     public static double[][] ctiData(int pocet_radku, BufferedReader br) throws IOException {
         double[][] data = new double[pocet_radku][];
 
@@ -211,36 +118,26 @@ public class du2 {
         }
         return data;
     }
-
-    /**
-     *
-     * @param data
-     * @param x
-     * @param y
-     * @param arguments
-     * @param lambda_jmenovatel
-     * @return
-     */
-    public static double spoctiVysledek(double[][] data, double x, double y, Arguments arguments, double lambda_jmenovatel) {
+    
+    public static double spoctiJmenovatel(double[][] data, double x, double y, Argumenty arguments) {
+        double lambda_jmenovatel = 0;
+        // výpočet jmenovatele lambdy
+        for (int i = 0; i < data.length; i++) {
+            double v = sqrt((x - data[i][0]) * (x - data[i][0]) + (y - data[i][1]) * (y - data[i][1]));
+            lambda_jmenovatel += 1 / Math.pow(v, arguments.p);
+        }
+        return lambda_jmenovatel;
+    }
+    
+    public static double spoctiVysledek(double[][] data, double x, double y, Argumenty arguments, double lambda_jmenovatel) {
         double z0 = 0;
         for (int i = 0; i < data.length; i++) {
             z0 = updateVysledku(x, data, i, y, arguments, lambda_jmenovatel, z0);
         }
         return z0;
     }
-
-    /**
-     *
-     * @param x
-     * @param data
-     * @param i
-     * @param y
-     * @param arguments
-     * @param lambda_spodni
-     * @param z0
-     * @return
-     */
-    public static double updateVysledku(double x, double[][] data, int i, double y, Arguments arguments, double lambda_spodni, double z0) {
+    
+    public static double updateVysledku(double x, double[][] data, int i, double y, Argumenty arguments, double lambda_spodni, double z0) {
         double lambda_horni;
         double lambda;
         double v = sqrt((x - data[i][0]) * (x - data[i][0]) + (y - data[i][1]) * (y - data[i][1]));
@@ -251,25 +148,38 @@ public class du2 {
         z0 += lambda * data[i][2];
         return z0;
     }
-
-    /**
-     *
-     * @return
-     * @throws IOException
-     */
-    public static int readInt() throws IOException {
-        BufferedReader reader;
-        reader = new BufferedReader(new InputStreamReader(System.in));
-        return Integer.parseInt(reader.readLine());
+    
+    public static double spoctiIDW(double[][] data, double x, double y, Argumenty arguments) {
+        double lambda_jmenovatel = spoctiJmenovatel(data, x, y, arguments);
+        //výpočet čitatele lambdy
+        double z0 = spoctiVysledek(data, x, y, arguments, lambda_jmenovatel); //writer.printf("%.2f, %.2f, %.2f\n", x, y, z0);
+        return z0;
     }
+    
+    public static void ulozVystupniSoubor(Argumenty argumenty, double[][] data) {
+        // uložení hodnot do určeného souboru
+        PrintWriter writer;
+        try {
+            writer = new PrintWriter("out.csv");
+            // vypíše počet 
+            writer.println(argumenty.sirka * argumenty.vyska);
+            // projde celý řádek od min s krokem dle definované hodnoty mřížky 'sirka' po max
+            for (double x = MIN_X; x <= MAX_X; x += (MAX_X - MIN_X) / argumenty.sirka) {
+                // projde všechny sloupce od min s krokem dle definované hodnoty mřížky 'vyska' po max
+                for (double y = MIN_Y; y <= MAX_Y; y += (MAX_Y - MIN_Y) / argumenty.vyska) {
 
-    /**
-     *
-     * @return
-     * @throws IOException
-     */
-    public static char readChar() throws IOException {
-        Scanner s = new Scanner(System.in);
-        return s.next().charAt(0);
+                    double z0 = spoctiIDW(data, x, y, argumenty);
+                    writer.printf("%.2f,", z0);
+                }
+                writer.printf("\n");
+            }
+            writer.close();
+        } catch (FileNotFoundException ex) {
+            System.err.format("Výstupní soubor nebyl nalezen!");
+            System.exit(1);
+        } catch (IOException ex) {
+            System.err.print("Vyskytla se chyba při ukládání výstupního souboru!");
+            System.exit(1);
+        }
     }
 }
